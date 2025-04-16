b3588bd489a728dbb0b778e24d6132789c4396d9
Ruslan <noname0263@gmail.com>
2025-04-17 00:21:04 +0900

    add exe bootstrap and GRACEFUL SHUTDOWN

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"github.com/lifedaemon-kill/burovichok-backend/internal/pkg/config"
 	"github.com/lifedaemon-kill/burovichok-backend/internal/pkg/logger/z"
 	"log"
+	"os/signal"
+	"syscall"
 )
 
 const configPath = "config/config.yaml"
 
 func main() {
-	fmt.Println("start app")
+	ctx := context.Background()
+	err := bootstrap(ctx)
+	if err != nil {
+		log.Fatalf("main bootstrap failed: %v", err)
+	}
+}
+
+func bootstrap(ctx context.Context) error {
+	ctx, cancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
 	conf := config.Load(configPath)
 
-	err := z.InitLogger(conf.Logger)
-	if err != nil {
+	if err := z.InitLogger(conf.Logger); err != nil {
 		log.Fatalf("init logger error: %s", err)
 	}
 	defer z.Log.Sync()
+	z.Log.Info("init logger and config success")
 
-	z.Log.Infow("zap logger init success")
+	/*
+		burApp := app.New()
+		burWindow := burApp.NewWindow("burovichok")
+		text := widget.NewLabel("hello, burovichok!")
+		burWindow.SetContent(text)
+		burWindow.Resize(fyne.NewSize(800, 400))
+		burWindow.ShowAndRun()
+	*/
+	<-ctx.Done()
+	z.Log.Info("context done, shutting down...")
 
-	z.Log.Infow("config load success", "conf", conf)
+	//	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	//	defer shutdownCancel()
 
-	fmt.Println("end")
+	//TODO завершить sqlite
+	z.Log.Info("shutting down completed")
+	return nil
 }
