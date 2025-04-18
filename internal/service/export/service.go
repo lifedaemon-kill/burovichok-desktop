6633548647189af615fcd52190a8014ee5c762f9
Ruslan <noname0263@gmail.com>
2025-04-18 12:52:20 +0900

    добавил сервис экспорта

--- internal/service/export/service.go
+++ internal/service/export/service.go
@@ -1,1 +1,23 @@
 package export
+
+import (
+	"github.com/lifedaemon-kill/burovichok-desktop/internal/pkg/models"
+	"github.com/lifedaemon-kill/burovichok-desktop/internal/storage/sqlite"
+)
+
+type Service interface {
+	SaveReport(
+		tableOne []models.TableOne,
+		tableTwo []models.TableTwo,
+		tableThree []models.TableThree,
+		tableFour []models.TableFour,
+		tableFive []models.TableFive) error
+
+	AddInstrumentType([]models.InstrumentType) error
+	AddOilField([]models.OilField) error
+	AddProductiveHorizon([]models.ProductiveHorizon) error
+}
+type dbService struct {
+	g sqlite.GuidebooksStorage
+	b sqlite.BlocksStorage
+}
