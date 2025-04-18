427812fef1dd8ea65c0c8e0e7f69e422b2496d85
H1DDENP00L <hidenpoolovski@gmail.com>
2025-04-18 13:39:53 +0700

    Дописал миграции. Migrations Done

--- migrations/20250418021459_create_table_three.sql
+++ migrations/20250418021459_create_table_three.sql
@@ -1,9 +1,18 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TABLE table_three (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    report_id INTEGER NOT NULL, -- Внешний ключ к reports.id
+    timestamp DATETIME NOT NULL,
+    flow_liquid REAL NOT NULL,
+    water_cut REAL NOT NULL,
+    flow_gas REAL NOT NULL,
+
+    FOREIGN KEY(report_id) REFERENCES reports(id) ON DELETE CASCADE
+);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DROP TABLE table_three;
 -- +goose StatementEnd
