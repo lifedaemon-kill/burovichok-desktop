dc2b856852554e9bdfb900549ac9b5e219df9294
Grigoryev Maksim Alekseevich <makgrigoryeva@ozon.ru>
2025-04-21 00:34:52 +0300

    поменял тип в миграции

--- migrations/20250420125044_insert_test_data.sql
+++ migrations/20250420125044_insert_test_data.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 CREATE TABLE table_four (
-    research_id                 INT             NOT NULL,
+    research_id                 UUID             NOT NULL,
     measure_depth               DOUBLE PRECISION NOT NULL,
     true_vertical_depth         DOUBLE PRECISION NOT NULL,
     true_vertical_depth_sub_sea DOUBLE PRECISION NOT NULL
